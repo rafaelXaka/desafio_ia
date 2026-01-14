b5c9139fe090435e7e7f4ead24e125d81bec6bc4
Rafael <rafaelcardozo.dev@gmail.com>
2026-01-14 17:27:31 -0300

    ajuste para deploy no render

--- app.py
+++ app.py
@@ -3,39 +3,53 @@ from flask import Flask, render_template, request
 from openai import OpenAI
 from dotenv import load_dotenv
 
-load_dotenv()
 
+load_dotenv()
 app = Flask(__name__)
 
-load_dotenv()
 
-api_key_env = os.getenv("OPENAI_API_KEY")
+minha_chave = os.getenv("OPENAI_API_KEY") or "sua_chave_aqui"
+client = OpenAI(api_key=minha_chave)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     classificacao = None
     resposta = None
     texto_original = None
-
+    
     if request.method == 'POST':
         texto_original = request.form.get('email_texto')
         
         if texto_original:
-            
-            prompt = f"Analise este email: '{texto_original}'. Responda estritamente no formato: CATEGORIA: [Produtivo ou Improdutivo] | RESPOSTA: [Sua sugestão]."
-            
-            response = client.chat.completions.create(
-                model="gpt-4o-mini",
-                messages=[{"role": "system", "content": "Você é um assistente bancário eficiente."},
-                          {"role": "user", "content": prompt}]
-            )
-            
-            resultado = response.choices[0].message.content
-            
-            classificacao = resultado.split('|')[0].replace("CATEGORIA:", "").strip()
-            resposta = resultado.split('|')[1].replace("RESPOSTA:", "").strip()
+            try:
+                
+                prompt = f"Analise o email: '{texto_original}'. Responda exatamente neste formato: CATEGORIA: [Produtivo ou Improdutivo] | RESPOSTA: [Sugestão de resposta]."
+                
+                response = client.chat.completions.create(
+                    model="gpt-4o-mini",
+                    messages=[
+                        {"role": "system", "content": "Você é um assistente de triagem bancária."},
+                        {"role": "user", "content": prompt}
+                    ]
+                )
+                
+                resultado = response.choices[0].message.content
+                
+                if "|" in resultado:
+                    partes = resultado.split('|')
+                    classificacao = partes[0].replace("CATEGORIA:", "").strip()
+                    resposta = partes[1].replace("RESPOSTA:", "").strip()
+                else:
+                    classificacao = "Análise Concluída"
+                    resposta = resultado
+
+            except Exception as e:
+                classificacao = "Erro"
+                resposta = f"Erro na API: {str(e)}"
 
     return render_template('index.html', classificacao=classificacao, resposta=resposta, texto=texto_original)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
